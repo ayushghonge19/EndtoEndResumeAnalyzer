3570218d145667ae6f8a8eccd64b8b0b5e1d7cfc
Ayush Ghonge <ayushghonge1309@gmail.com>
2025-09-04 23:34:15 +0530

    Nav bar banadi hain

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Best Feedback for your Dream Job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <p>Review your submissions and check AI-powered feedback.</p>
+      </div>
+    </section>
+    {}
+  </main>
 }
